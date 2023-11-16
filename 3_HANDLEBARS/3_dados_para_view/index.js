61ff8253272e1edc4fefbdaa896017cfc3271a8a
brunosampaio222 <brunosampaio2223@gmail.com>
2023-11-16 20:50:55 -0300

    NODE-AULAS

---
+++ 3_HANDLEBARS/3_dados_para_view/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// configurando o handlebars como template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    const usuario= {
+        nome: "bruno",
+        profissao: "Programador",
+        idade: 21
+    }
+    res.render('home',{ usuario})
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
